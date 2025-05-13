825f701124b7df2d1889b57c4556f3f112c45319
gretagulyas07 <ggretus07@gmail.com>
2025-05-13 22:13:27 +0200

    Login oldal módosítása

--- Views/BurgerSize.xaml.cs
+++ Views/BurgerSize.xaml.cs
@@ -3,14 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
 
 namespace WpfBurgerApp.Views
 {
-    internal class BurgerSize
+    /// <summary>
+    /// Interaction logic for BurgerSize.xaml
+    /// </summary>
+    public partial class BurgerSize : Window
     {
-        internal void Show()
+        public BurgerSize()
         {
-            throw new NotImplementedException();
+            InitializeComponent();
         }
     }
 }
--- obj/Debug/net8.0-windows/WpfBurgerApp.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/WpfBurgerApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WpfBurgerApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86eeaa1e8dc01537b69d91f23a5d4128dd4e1825")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("WpfBurgerApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WpfBurgerApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
